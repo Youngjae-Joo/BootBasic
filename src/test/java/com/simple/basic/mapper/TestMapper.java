1a6c421a3e6557d16e7d18b1cf0c35280c38ae62
Youngjae-Joo <joo0jae@gmail.com>
2023-02-20 19:56:07 +0900

    0220

--- src/test/java/com/simple/basic/mapper/TestMapper.java
+++ src/test/java/com/simple/basic/mapper/TestMapper.java
@@ -1,10 +1,21 @@
 package com.simple.basic.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.simple.basic.command.MemoVO;
+import com.simple.basic.command.UsersVO;
 
 @Mapper//스프링 부트에서는 매퍼 인터페이스에 매퍼 어노테이션을 반드시 붙여야 한다.
 public interface TestMapper {
 
 	public String getTime();
+	
+	//N:1조인의 모형. ex)글-회원정보
+	public List<MemoVO> joinOne();
+	
+	//1:N조인의 모형. ex)회원정보-글
+	public UsersVO<MemoVO> joinTwo();
 	
 }
